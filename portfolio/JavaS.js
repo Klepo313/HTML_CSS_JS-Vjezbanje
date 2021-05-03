9ed1c3d7ebd6b9124655bc218638b61209edf6fc
TheKlepo75 <antonio.klepo1@gmail.com>
2021-05-03 11:24:46 +0200

    commit

--- portfolio/JavaS.js
+++ portfolio/JavaS.js
@@ -1,7 +1,5 @@
 
 var btnPlay = document.getElementById("btnPlayVideo");
-function sirinaVisina(){
-    alert("Širina: " + window.screen.availWidth + "px\n" + "Visina: " + window.screen.availHeight + "px");
-}
 
-btnPlay.addEventListener("click", sirinaVisina);
+
+//btnPlay.addEventListener("click", );
