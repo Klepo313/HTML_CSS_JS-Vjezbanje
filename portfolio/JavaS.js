963fc1f1da60ab254483e9829bf113bd968d836d
TheKlepo75 <antonio.klepo1@gmail.com>
2021-05-17 17:28:05 +0200

    commit

--- portfolio/JavaS.js
+++ portfolio/JavaS.js
@@ -1,7 +1,7 @@
 
 var btnPlay = document.getElementById("btnPlayVideo");
-
 var sideArrow = document.getElementById("arr-right");
+var s2t = document.getElementById("scroll2TopBtn");
 let burgerMenu = document.getElementsByClassName("burger");
 let sidebar = document.getElementsByClassName("sidebar");
 
@@ -15,3 +15,20 @@ function hideSideMenu(){
 burgerMenu[0].addEventListener("click", showSideMenu, false);
 
 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    s2t.style.display = "block";
+  } else {
+    s2t.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topPosition() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
